1e9d7465cda75a2fcc1c8a85fafa7ec764d55cbc
amaan-p <amaanmpatelemail@gmail.com>
2023-11-07 11:19:24 +0530

    feat: changes to file

---
+++ lib/secret.dart
@@ -0,0 +1,4 @@
+class Secret {
+  static const String supabaseUrl = 'https://kwypawgdpzmezidelkvo.supabase.co';
+  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt3eXBhd2dkcHptZXppZGVsa3ZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODgwMTI5MjMsImV4cCI6MjAwMzU4ODkyM30.NXeWOn88TBg4bLDQzJUfVVn85t5AVVt0JVP-jCgRY_M';
+}
